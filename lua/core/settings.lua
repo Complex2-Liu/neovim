b75af19d6d04a6743e6b91ea5ecb98e442ca58b7
complex2liu <contact@complex2math.com>
2022-07-15 23:10:58 +0800

    core.settings: update

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -4,12 +4,22 @@ local options = {
   relativenumber = true,
 
   mouse = "a",  -- enable mouse support for all mode
+  cursorline = true, -- highlight the current line
+
+  -- Minimal number of screen lines to keep above and below the cursor
+  scrolloff = 8,
+  sidescrolloff = 8,
+
 
   -- replace <Tab> by appropriate number of spaces
   -- To insert a real <Tab>, use <CTRL-V> + <Tab>
   expandtab = true,
   tabstop = 2,  -- number of spaces that a <Tab> counts for
   shiftwidth = 2,
+
+  -- Time in milliseconds to wait for a mapped sequence to complete
+  -- This is used by which-key plugin
+  timeoutlen = 500,
 
   -- menuone: Use the popup menu also when there is only one match
   -- noselect: Do not select a match in the menu, force the user to select one from the menu
